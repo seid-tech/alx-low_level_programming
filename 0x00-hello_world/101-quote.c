01092ce682f631085a3ac0d716c533e881e9d366
seid-tech <seid.jabir.nurie@gmail.com>
2022-07-07 05:33:11 -0700

    ok

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,11 @@
 #include <unistd.h>
 #include <stdio.h>
 
+/**
+ * main - This program prints a sentence and a date, followed
+ * by a new line, to the standard error
+ * Return: 1 for success
+ */
 
 int main(void)
 
